8a3efacd4cee0f559df10ba9e7f740fb318b3139
Leandro-Casaretto <leandrodcasaretto@gmail.com>
2022-12-19 08:54:58 -0300

    Desafío 4

--- src/Library/ConsolePrinter.cs
+++ src/Library/ConsolePrinter.cs
@@ -5,6 +5,10 @@ namespace Full_GRASP_And_SOLID.Library
     
     public class ConsolePrinter : IPrinter
     {
+        /// <summary>
+        /// Ahora que aplicamos DIP, en vez de pasarle como parámetro un Building, le pasamos como
+        /// parámetro un objeto de tipo IPrintable.
+        /// </summary>
         public void PrintBuilding(IPrintable textPrintable)
         {
             Console.WriteLine(textPrintable.GetTextToPrint());
